a7037c77e1694a2334aa4a7d9807708861d3e637
Rafael PacMe <rafy.pachecomendez@gmail.com>
2016-06-26 16:34:22 -0300

    Update and rename creeper.mk to cm.mk

--- cm.mk
+++ cm.mk
@@ -6,14 +6,14 @@ TARGET_SCREEN_HEIGHT := 1024
 TARGET_SCREEN_WIDTH := 600
 
 # Inherit some common CREEPER stuff.
-$(call inherit-product, vendor/creeper/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/lt02wifi/device_lt02wifi.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lt02wifi
-PRODUCT_NAME := creeper_lt02wifi
+PRODUCT_NAME := cm_lt02wifi
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := SM-T210
 PRODUCT_MANUFACTURER := samsung
